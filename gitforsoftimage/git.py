0b651c6af67d5db5cd5809773d0bfa57a4898fe8
csaez <cesarte@gmail.com>
2013-07-05 16:32:11 -0400

    git wrapper moved to its own file

---
+++ gitforsoftimage/git.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+from collesctions import namedtuple
+
+
+def git(*args, **kwds):
+    # get command args
+    cmd = ["git"]
+    cmd.extend(list(args))
+    # if there's not an explicit cwd, look for a filepath on the args
+    if not kwds.get("cwd"):
+        for filepath in args:
+            if not os.path.exists(filepath):
+                continue
+            # set current working directory
+            kwds["cwd"] = os.path.dirname(filepath)
+    # set subprocess
+    kwds["stdout"] = subprocess.PIPE
+    kwds["stderr"] = subprocess.PIPE
+    kwds["shell"] = True  # hide shell
+    proc = subprocess.Popen(cmd, **kwds)
+    # get log and return value
+    stdout, stderr = proc.communicate()
+    value = proc.wait()
+    # return a namedtuple with the results
+    results = namedtuple("git_call", "value, stdout, stderr")
+    return results._make((value, stdout, stderr))
--- gitforsoftimage/manager.py
+++ gitforsoftimage/manager.py
@@ -1,39 +1,15 @@
-from collections import namedtuple
-import subprocess
 import os
-import json
 import re
+import json
 import shutil
+from collections import namedtuple
 
 from PyQt4.QtGui import QMessageBox
 from wishlib.si import sianchor
 from wishlib.qt.QtGui import QDialog
 
+from ..git import git
 from .layout.gitdialog import GitDialog
-
-
-def git(*args, **kwds):
-    # get command args
-    cmd = ["git"]
-    cmd.extend(list(args))
-    # if there's not an explicit cwd, look for a filepath on the args
-    if not kwds.get("cwd"):
-        for filepath in args:
-            if not os.path.exists(filepath):
-                continue
-            # set current working directory
-            kwds["cwd"] = os.path.dirname(filepath)
-    # set subprocess
-    kwds["stdout"] = subprocess.PIPE
-    kwds["stderr"] = subprocess.PIPE
-    kwds["shell"] = True  # hide shell
-    proc = subprocess.Popen(cmd, **kwds)
-    # get log and return value
-    stdout, stderr = proc.communicate()
-    value = proc.wait()
-    # return a namedtuple with the results
-    results = namedtuple("git_call", "value, stdout, stderr")
-    return results._make((value, stdout, stderr))
 
 
 class Manager(object):
